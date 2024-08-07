11951d0d9a944705642e1731d33868ca17aec8ea
thejbordo <joan.bordonaba@gmail.com>
2024-08-08 01:55:41 +0200

    feat: sdfsd

--- web/src/data/dataStuff.ts
+++ web/src/data/dataStuff.ts
@@ -24,7 +24,7 @@ export const stuffs = [
     nameState: "barrios",
   },
   {
-    url: "public/joan.webp",
+    url: "/joan.webp",
     name: "Joan Bordonaba",
     title: "CTO",
     description:
